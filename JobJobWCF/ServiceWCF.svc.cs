cdf88c38a9cb4f23b3f48898777109d6009d0b00
Axel <axelbruget@gmail.com>
2019-06-24 10:26:10 +0200

    WCF working

---
+++ JobJobWCF/ServiceWCF.svc.cs
@@ -0,0 +1,31 @@
+﻿using JobJobClass.Entity;
+using JobJobService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace JobJobWCF
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service" dans le code, le fichier svc et le fichier de configuration.
+    public class ServiceWCF : IServiceWCF
+    {
+        public List<OffreDataContract> getOffres()
+        {
+            Service service = new Service();
+            List<Offre> listOffres = service.getOffres();
+
+            List<OffreDataContract> listOffreDataContract = new List<OffreDataContract>();
+
+            foreach (Offre offre in listOffres)
+            {
+                OffreDataContract offreDataContract = new OffreDataContract(offre);
+                listOffreDataContract.Add(offreDataContract);
+            }
+            return listOffreDataContract;
+        }
+    }
+}
