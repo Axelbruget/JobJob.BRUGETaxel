654bfe0a77282cb94fcfe1700f25ef0ff9c2e1cf
Axel <axelbruget@gmail.com>
2019-06-28 17:08:16 +0200

    added unit test, end project

---
+++ JobJobTest/UnitTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using JobJobClass.Entity;
+using JobJobClass.Mapping;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JobJobService;
+using System.Collections.Generic;
+
+namespace JobJobTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        Service service = new Service();
+
+        Offre offre = new Offre
+        {
+            Date = DateTime.Today,
+            Intitule = "Mon offre",
+            Responsable = "Moi",
+            Salaire = 25000,
+            StatutId = 1,
+            Description = "Ma description",
+        };
+
+        [TestMethod]
+        public void AddOffre()
+        { 
+            service.addOffre(offre);
+
+            Assert.IsNotNull(service.getOffre(offre.Id));
+        }
+
+        [TestMethod]
+        public void UpdateOffre()
+        {
+            service.addOffre(offre);
+
+            offre.Intitule = "Ma nouvelle offre";
+            service.updateOffre(offre);
+
+            Assert.AreEqual(service.getOffre(offre.Id).Intitule, "Ma nouvelle offre");
+        }
+
+        [TestMethod]
+        public void DeleteOffre()
+        {
+            
+            service.addOffre(offre);
+            service.removeOffre(offre);
+
+            Assert.IsNull(service.getOffre(offre.Id));
+
+        }
+
+        [TestMethod]
+        public void GetOffres()
+        {
+            service.addOffre(offre);
+
+            Assert.IsNotNull(service.getOffres());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+
+Réalisé :
+
+Les entités en utilisant les Data Annotation
+Tests unitaires sur les Offres seulement ( Ajout, Suppression, Get et Modification)
+la couche d'accès aux données
+Application WPF avec à gauche la liste des offres et le détail à droite
+Application WCF de type REST, le GET sur la liste des offres fonctionne (http://localhost:58395/ServiceWCF.svc/rest/getOffres)
+Application ASP.NET avec le CRUD sur les offres qui fonctionne
+
+Manquant : 
+
+L'application WPF : 
+- ne liste pas la liste des postulants
+- On ne peut pas modifier un salaire
+
+ASP.NET : 	
+- pouvoir postuler sur une offre
+- voir les postulations de l'utilisateur connecté
+- champs de recherche
+
